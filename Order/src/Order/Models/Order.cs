80b89e40a84cdca9fb990bfda89c7b79bc776643
jansan mariyathas <jansan-26@hotmail.com>
2016-02-10 11:53:41 +0100

    Start upp

---
+++ Order/src/Order/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.Models
+{
+    public class Order
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
